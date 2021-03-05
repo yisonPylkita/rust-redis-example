45c5dc37b1e365207a5535c4c7787b539d9194f8
Wojciech Bartnik <yisonPylkita@gmail.com>
2021-03-05 13:11:02 +0100

    Dead simple SET

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn connect() -> redis::Connection {
+    redis::Client::open("redis://:@localhost".to_string())
+        .expect("Invalid connection URL")
+        .get_connection()
+        .expect("failed to connect to Redis")
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut redis_handle = connect();
+    let _: () = redis::cmd("SET")
+        .arg("measurand")
+        .arg("10")
+        .query(&mut redis_handle)
+        .expect("failed to execute SET for 'measurand'");
 }
